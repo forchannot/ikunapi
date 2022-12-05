1bef643ab36b921f89100acd73032d179d64d247
forchannot <yy320206@gmail.com>
2022-12-05 11:42:48 +0800

    update

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 //初始化随机数生成器种子，这行代码也可以删除
 $seed = time();
 //获取随机数
-$num = rand(1,45);
+$num = rand(1,70);
 //拼接图片地址
 $picpath = "https://ghproxy.com/https://raw.githubusercontent.com/forchannot/ikunapi/main/ikun/".$num.".jpg";
 //重定位到图片
